2c6ac281c1210341e10e90e6f54aee10169d11bc
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-19 19:17:53 +0100

    Add util views

---
+++ app/src/main/java/com/equijada95/randomusercodetext/presentation/utilities/SearchBar.kt
@@ -0,0 +1,62 @@
+package com.equijada95.randomusercodetext.presentation.utilities
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import com.equijada95.randomusercodetext.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun SearchBar(
+    setSearch: (String) -> Unit
+) {
+    var searchText by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = ""
+            )
+        },
+        trailingIcon = {
+            if (searchText.isNotEmpty()) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clickable(
+                            onClick = {
+                                searchText = ""
+                                setSearch(searchText)
+                            }
+                        )
+                )
+            }
+        },
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.padding_constraint))
+            .fillMaxWidth(),
+        value = searchText,
+        onValueChange = {
+            searchText = it
+            setSearch(it)
+        },
+        shape = RoundedCornerShape(dimensionResource(id = R.dimen.search_bar_corner_radius)),
+        label = { Text(stringResource(id = R.string.search)) }
+    )
+}
