a8babb0a66fea363c6eca7dd2630edbd4ce61e79
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-19 17:03:19 +0100

    Add hilt and data dependecy

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -33,6 +35,12 @@ android {
 }
 
 dependencies {
+    // Modules
+    implementation(project(":data"))
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
