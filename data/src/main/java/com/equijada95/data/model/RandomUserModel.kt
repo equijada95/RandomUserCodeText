9f7adb7e448ef79ab13c9e830e3181b335e875d2
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-19 17:03:28 +0100

    Update RandomUserModel.kt

--- data/src/main/java/com/equijada95/data/model/RandomUserModel.kt
+++ data/src/main/java/com/equijada95/data/model/RandomUserModel.kt
@@ -4,9 +4,9 @@ import com.equijada95.data.model.location.LocationModel
 
 data class RandomUserModel(
     val name: NameModel,
-    val locationModel: LocationModel,
+    val location: LocationModel,
     val email: String,
     val gender: String,
-    val pictureModel: PictureModel,
+    val picture: PictureModel,
     val registered: RegisteredModel
 )
