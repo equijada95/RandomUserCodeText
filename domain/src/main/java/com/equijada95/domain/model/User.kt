1bb83ed2e34c4a2dc745cd29c4aeb29d3fc6e475
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-19 17:03:36 +0100

    Create User.kt

---
+++ domain/src/main/java/com/equijada95/domain/model/User.kt
@@ -0,0 +1,11 @@
+package com.equijada95.domain.model
+
+data class User(
+    val name: String,
+    val latitude: String,
+    val longitude: String,
+    val email: String,
+    val gender: String,
+    val picture: String,
+    val registeredDate: String
+)
