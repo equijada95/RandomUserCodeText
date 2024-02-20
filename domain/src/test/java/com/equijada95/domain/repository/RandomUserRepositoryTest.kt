e729db517e70d1818dcea745bb2f69931607ea4a
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-20 20:52:55 +0100

    Add more test

--- domain/src/test/java/com/equijada95/domain/repository/RandomUserRepositoryTest.kt
+++ domain/src/test/java/com/equijada95/domain/repository/RandomUserRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.equijada95.domain.repository
 
+import android.net.http.HttpException
 import app.cash.turbine.test
 import com.equijada95.data.model.NameModel
 import com.equijada95.data.model.PictureModel
@@ -14,7 +15,10 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody
 import org.junit.jupiter.api.Test
+import retrofit2.Response
+import java.io.IOException
 import kotlin.test.assertEquals
 
 class RandomUserRepositoryTest {
@@ -66,6 +70,9 @@ class RandomUserRepositoryTest {
         )
     )
 
+    private val httpException = mockk<HttpException>(relaxed = true)
+    private val ioException = mockk<IOException>(relaxed = true)
+
     @Test
     fun `get all users`() {
         coEvery { provider.getResults(any()).body() } returns mockUsers
@@ -75,6 +82,36 @@ class RandomUserRepositoryTest {
                 val response = awaitItem()
                 assert(response is ApiResult.Success)
                 assertEquals(response.data, mockUsers.results.toUserList())
+                coVerify { provider.getResults(any()) }
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `when an server error occurs, returns an error`() {
+        coEvery { provider.getResults(any()) } returns Response.error(404, mockk<ResponseBody>(relaxed = true))
+
+        runBlocking {
+            repository.getUsers(2).test {
+                val response = awaitItem()
+                assert(response is ApiResult.Error)
+                assertEquals(response.error, ApiResult.ApiError.SERVER_ERROR)
+                coVerify { provider.getResults(any()) }
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `when a no connection error occurs, returns an error`() {
+        coEvery { provider.getResults(any()).body() } throws ioException
+
+        runBlocking {
+            repository.getUsers(2).test {
+                val response = awaitItem()
+                assert(response is ApiResult.Error)
+                assertEquals(response.error, ApiResult.ApiError.NO_CONNECTION_ERROR)
                 coVerify { provider.getResults(any()) }
                 cancelAndConsumeRemainingEvents()
             }
