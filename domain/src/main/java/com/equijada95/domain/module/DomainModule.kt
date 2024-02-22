1b8c7a141f1bb2f070290f27fad7e1a960e7d3c9
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2024-02-22 18:13:35 +0100

    Advances

--- domain/src/main/java/com/equijada95/domain/module/DomainModule.kt
+++ domain/src/main/java/com/equijada95/domain/module/DomainModule.kt
@@ -4,6 +4,8 @@ import com.equijada95.data.model.RandomUserResults
 import com.equijada95.domain.mapper.BaseMapper
 import com.equijada95.domain.mapper.user.UserMapper
 import com.equijada95.domain.model.User
+import com.equijada95.domain.repository.RandomUserRepository
+import com.equijada95.domain.repository.RandomUserRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +14,14 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class UserMapperModule {
+class DomainModule {
 
     @Provides
     @Singleton
-    fun providerRandomUserRepository(userMapper: UserMapper): BaseMapper<RandomUserResults, List<User>> = userMapper
+    fun providesUserMapper(userMapper: UserMapper): BaseMapper<RandomUserResults, List<User>> = userMapper
+
+    @Provides
+    @Singleton
+    fun providesRandomUserRepository(randomUserRepositoryImpl: RandomUserRepositoryImpl): RandomUserRepository = randomUserRepositoryImpl
 
 }
